e0b14c12fba90f439bbf518305021347cec17b8b
Galfi Istvan Balazs <112966@fhwn.ac.at>
2021-10-09 17:34:08 +0200

    Soon it is done.

---
+++ Lab1/app/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (_, res) {
+  res.send('Hello World')
+})
+ 
+const port = 443;
+app.listen(port, "", () => console.log(`Listening on ${port}`))
