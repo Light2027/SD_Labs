5f8d5da903a39c3947ae772b30283a95e1bb68c9
Galfi Istvan Balazs <112966@fhwn.ac.at>
2021-10-12 20:05:03 +0200

    Now it should be done.

--- Lab1/deploymentScript.sh
+++ Lab1/deploymentScript.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 echo "Creating Resource Group"
 resourceGroupName=SD_Labs
-az group create --location westeurope --name $resourceGroupName
+location=westeurope
+az group create --location $location --name $resourceGroupName #--debug
 
 echo "Creating deployment group"
 deploymentName=Lab-1
 templateFileName=azuredeploy.json
 parameterFileName='@azuredeploy.parameters.json'
+webAppName=myWebApp875
 az deployment group create \
   --name $deploymentName \
   --resource-group $resourceGroupName \
   --template-file $templateFileName \
-  --parameters $parameterFileName
+  --parameters $parameterFileName \
+  --parameters "webAppName=$webAppName" #--debug # This is passed in here as source control magically does not work as resource...
 
 echo "Deploying web app from github"
-webappname=myWebApp875
 gitrepo=https://github.com/Light2027/Node.JS_HelloWorld_Azure_CI
 branch=main
-az webapp deployment source config --name $webappname --resource-group $resourceGroupName \
---repo-url $gitrepo --branch $branch --manual-integration
+az webapp deployment source config \
+--name $webAppName \
+--resource-group $resourceGroupName \
+--repo-url $gitrepo --branch $branch --manual-integration #--debug
